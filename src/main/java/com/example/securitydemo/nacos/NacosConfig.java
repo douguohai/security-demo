4148e3e4f99d322f9c61b9ca19fe00d692d3d5f7
douguohai <douguohai@163.com>
2020-12-31 09:49:20 +0800

    测试nacos配置代码

---
+++ src/main/java/com/example/securitydemo/nacos/NacosConfig.java
@@ -0,0 +1,32 @@
+package com.example.securitydemo.nacos;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version : 1.0
+ * @description: java类作用描述
+ * @author: tianwen
+ * @create: 2020/12/31 09:09
+ **/
+@NacosPropertySource(dataId = "escortsys", autoRefreshed = true)
+@RestController
+public class NacosConfig {
+
+    @NacosValue(value = "${wx.pay.key}", autoRefreshed = true)
+    private String useLocalCache;
+
+    @NacosValue(value = "${wx.pay.cert}", autoRefreshed = true)
+    private String cert;
+
+    @RequestMapping(value = "/get")
+    @ResponseBody
+    public boolean get() {
+        System.out.printf(useLocalCache);
+        System.out.printf(cert);
+        return true;
+    }
+}
